8a7214f89c6b7198d4ff6c141e8497fdd24ac2aa
Amitai Yacobi <amitai@Amitais-MacBook-Pro.local>
2021-12-12 12:25:56 +0200

    bug fixes. current best accuracy on test_set - 97.8%

--- ex3.py
+++ ex3.py
@@ -1,7 +1,12 @@
 import sys
 import numpy as np
 
-    
+def split_data(data, lines_of_train):
+    train_set = data[ :lines_of_train]
+    validation_set = data[lines_of_train: ]
+    return train_set, validation_set
+
+
 def zscore_normalization(data_set):
     return (data_set - data_set.mean(0)) / data_set.std(0)
 
@@ -19,27 +24,23 @@ class NN:
     def weights_and_bias_init(self):
         layers_size = self.layers_size
         W1 = np.random.randn(layers_size[1], layers_size[0]) * 0.01
-        b1 = np.random.randn(layers_size[1], 1) * 0.01
+        b1 = np.zeros((layers_size[1], 1))
         W2 = np.random.randn(layers_size[2], layers_size[1]) * 0.01
-        b2 = np.random.randn(layers_size[2], 1) * 0.01
+        b2 = np.zeros((layers_size[2], 1))
         self.best_params =  {'W1' : W1, 'b1': b1, 'W2': W2, 'b2' :b2}
         
-    def feed_forward(self, x, target, params):
+    def feed_forward(self, x, params):
         W1, b1, W2, b2 = [params[key] for key in ('W1', 'b1', 'W2', 'b2')]
-        is_correct_prediction = False
         z1 = np.dot(W1, x) + b1
         h1 = self.sigmoid(z1)
         z2 = np.dot(W2, h1) + b2
         h2 = self.softmax(z2)
-        predicted_result = np.where(h2 == h2.max())[0][0] + 1
-        if predicted_result == target:
-            is_correct_prediction = True
-        return {'x':x, 'z1':z1, 'h1':h1, 'z2':z2, 'h2':h2, 'is_correct_prediction': is_correct_prediction}
+        predicted_result = np.argmax(h2)
+        return {'x':x, 'z1':z1, 'h1':h1, 'z2':z2, 'h2':h2, 'predicted_result': predicted_result}
 
     def back_prop(self, cache_from_forward, target, params):
-        y = np.zeros(10)
-        y[int(target) - 1] = 1
-        y = y.reshape(len(y), 1)
+        y = np.zeros((10,1))
+        y[int(target)] = 1
         x = cache_from_forward['x']
         h2 = cache_from_forward['h2']
         z2 = cache_from_forward['z2']
@@ -50,8 +51,6 @@ class NN:
         dW2 = np.dot(dz2, h1.T)
         db2 = dz2
         dz1 = np.dot(params['W2'].T, dz2) * self.sigmoid_derivative(z1)
-        dW1 = np.dot(dz1, x.T)
-        db1 = dz1
         dW1 = np.dot(dz1, x.T)
         db1 = dz1
 
@@ -65,30 +64,40 @@ class NN:
         counter = 0
         for input_line, target in validation_set:
             input_line = input_line.reshape(len(input_line), 1)
-            cache_from_forward = self.feed_forward(input_line, target, params)
-            is_correct_result = cache_from_forward['is_correct_prediction']
-            if is_correct_result == True:
+            cache_from_forward = self.feed_forward(input_line, params)
+            predicted_result = cache_from_forward['predicted_result']
+            if predicted_result == int(target):
                 counter += 1
         accuracy = (counter / len(validation_set)) * 100
         return accuracy
 
     def train(self, train_set, validation_set, num_of_epochs):
         self.weights_and_bias_init()
-        params = self.best_params
         for i in range(num_of_epochs):
+            params = self.best_params.copy()
+            np.random.shuffle(train_set)
             for input_line, target in train_set:
                 input_line = input_line.reshape(len(input_line), 1)
-                cache_from_forward = self.feed_forward(input_line, target, params)
-                params = self.back_prop(cache_from_forward, target, params)
+                cache_from_forward = self.feed_forward(input_line, params).copy()
+                params = self.back_prop(cache_from_forward, target, params).copy()
             accuracy = self.validate(validation_set, params)
             print(accuracy)
             if accuracy >= self.current_accuracy:
                 self.current_accuracy = accuracy
-                self.best_params = params
+                self.best_params = params.copy()
+        return params
+
+    def predict(self, test_set, params):
+        test_y = open("test_y", 'w')
+        for input_line in test_set:
+            input_line = input_line.reshape(len(input_line), 1)
+            cache_from_forward = self.feed_forward(input_line, params)
+            predicted_result = cache_from_forward['predicted_result']
+            test_y.write(str(predicted_result) + '\n')
 
     def softmax(self, x):
-        e = np.exp(x - np.max(x))
-        return e / e.sum()
+        exps = np.exp(x - x.max())
+        return exps / np.sum(exps, axis=0)
 
     def Relu(self, x):
         return np.maximum(0, x)
@@ -104,27 +113,27 @@ if __name__ == "__main__":
     train_data = np.loadtxt(sys.argv[1])
     targets = np.loadtxt(sys.argv[2])
     test_data = np.loadtxt(sys.argv[3])
- 
+    test_targets = np.loadtxt("real_data/test_y")
+
     train_data = train_data / 255 # normalization
+    test_data = test_data / 255
+    
+    union_train_data = list(zip(train_data,  targets))
+    np.random.shuffle(union_train_data)
 
-    train_set = train_data[: 2000]
-    validation_set = train_data[2000: ]
+    union_test_data = list(zip(test_data, test_targets))
 
-    targets_for_train = targets[: 2000]
-    targets_for_validation = targets[2000: ]
-
-    union_train_set = list(zip(train_set,  targets_for_train))
-    np.random.shuffle(union_train_set)
-
-    union_validation_set = list(zip(validation_set,  targets_for_validation))
-    np.random.shuffle(union_validation_set)
+    # train_set ,validation_set = split_data(union_train_data, 49500)
+    train_set = union_train_data
+    test_set = union_test_data
 
     NN = NN()
     NN.create_layer(len(train_data[0]))   # input
-    NN.create_layer(100)                  # hidden
+    NN.create_layer(128)                  # hidden
     NN.create_layer(10)                   # output
     
-    NN.train(union_train_set, union_validation_set, 5)
+    params = NN.train(train_set, test_set, 14)
+    # NN.predict(test_data, params)
  
 
 
